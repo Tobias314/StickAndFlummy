b154e4a9acc353a85c84b468dfa576472a44e878
Isabell <isi@Isabells-MacBook-Pro.local>
2020-02-29 17:30:08 +0100

    Some collision point debugging.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,14 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public bool isJumping;
+    public bool isColliding;
+
+    private Vector3[] directions = new Vector3[]{ new Vector3(1,0,0),
+                        new Vector3(-1,0,0),
+                        new Vector3(0,-1,0),
+                        new Vector3(0,1,0),
+                        new Vector3(0,0,1),
+                        new Vector3(0,0,-1)};
 
 
     void Start()
@@ -18,17 +26,35 @@ public class PlayerController : MonoBehaviour
     void Update(){
         if(!isJumping){
             isJumping = Input.GetButtonDown("Jump");
+            
         }
-        
+        //Physics.Raycast(transform.position,)
     }
 
     void FixedUpdate()
     {
-        Debug.Log(isJumping);
+        //Debug.Log(isJumping);
         rb.MovePosition(transform.position + transform.right * Time.fixedDeltaTime);
         if(isJumping){
-            rb.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
             isJumping=false;
+            if(isColliding){
+                rb.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
+            }
         }
     }
+
+    private void OnCollisionEnter(Collision collision){
+        //Debug.Log("entered");
+        isColliding = true;
+        Debug.Log("number of contact points" + collision.contactCount);
+        for(int i=0; i<collision.contactCount; i++){
+            Debug.Log(collision.GetContact(i).point);
+        }
+    }
+
+    private void OnCollisionExit(Collision other){
+        //Debug.Log("exited");
+        isColliding = false;
+    }
+
 }
