be51df49143db3493af835d9b1fd401912451d14
Isabell <isi@Isabells-MacBook-Pro.local>
2020-02-29 13:57:19 +0100

    es hüpft

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool isJumping;
+
+
+    void Start()
+    { 
+        rb = GetComponent<Rigidbody>();
+
+        // Moves the GameObject using it's transform.
+        //rb.isKinematic = true;
+    }
+
+    void Update(){
+        if(!isJumping){
+            isJumping = Input.GetButtonDown("Jump");
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(isJumping);
+        rb.MovePosition(transform.position + transform.right * Time.fixedDeltaTime);
+        if(isJumping){
+            rb.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
+            isJumping=false;
+        }
+    }
+}
